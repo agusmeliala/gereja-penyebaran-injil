aa507e73937f223a63fa669015bee3269ea345d4
agusmeliala <aguschandra366@gmail.com>
2025-08-31 17:51:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(res => res.json())
     .then(data => {
       const hariList = ["Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
-      const today = new Date().getDay(); // 0=minggu
+      const today = new Date().getDay();
       const renungan = data.find(r => r.hari === hariList[today]);
       if (renungan) {
         document.getElementById("renungan-ayat").textContent = renungan.ayat;
